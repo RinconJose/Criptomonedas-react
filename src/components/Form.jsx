2715b4dd46e90d017f25f97516dd4daa427b5f8f
Jose Rincon <rincon.jmg@gmail.com>
2022-03-11 05:51:44 -0500

    create first custom hook

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,5 +1,10 @@
 import React from 'react'
 import styled from '@emotion/styled'
+import useSelectMonedas from '../hooks/useSelectMonedas'
+
+const [ SelectMonedas ] = useSelectMonedas();
+
+SelectMonedas();
 
 const Form = () => {
 
