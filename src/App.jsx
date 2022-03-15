4a808ff5b4aa948f7ed7d00a55b05c11888a1062
Jose Rincon <rincon.jmg@gmail.com>
2022-03-14 22:01:00 -0500

    Adding spinner

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useState, useEffect } from 'react'
 import styled from '@emotion/styled'
 import Form from './components/Form'
 import Resultado from './components/Resultado'
+import Spiner from './components/Spiner'
 import ImagenCripto from './img/imagen-criptos.png'
 
 const Container = styled.div`
@@ -46,16 +47,20 @@ function App() {
 
   const [ monedas, setMonedas ] = useState({})
   const [ resultado, setResultado ] = useState({})
+  const [ cargando, setCargando ] = useState(false)
 
   useEffect(() => {
     if(Object.keys(monedas).length > 0) {
       const cotizarCripto = async () => {
+        setCargando(true)
+        setResultado({})
         const { moneda, criptomoneda } = monedas;
         const url = `https://min-api.cryptocompare.com/data/pricemultifull?fsyms=${criptomoneda}&tsyms=${moneda}`;
         const respuesta = await fetch(url)
         const resultado = await respuesta.json()
 
         setResultado(resultado.DISPLAY[criptomoneda][moneda])
+        setCargando(false)
       }
 
       cotizarCripto()
@@ -73,7 +78,7 @@ function App() {
         <Form 
           setMonedas={setMonedas}
         />
-
+        {cargando && <Spiner />}
         {resultado.PRICE && <Resultado resultado={resultado}/>}
       </div>
     </Container>
